498c1191c0f5384d9a22b1d007fa01c34aaa7bfa
eedeidk <59429783+eedeidk@users.noreply.github.com>
2023-08-02 18:58:13 +0530

    some ui imprv

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from tools.helpers import *
 from tools.parsers import *
 
+softinfo = 'CGLSCORE'
+author = 'https://github.com/eedeidk/cglscore'
+
+print(f'Welcome to {softinfo}')
+
 url = str(input("Paste scorecard URL: "))	
 resp = getpage(url=url)
 exam=str(input('Is this CGL? (y/n)'))
@@ -10,3 +15,5 @@ if not exam.lower() == 'y':
     if n<0:
         n=n*(-1)
 Parseme(resp=resp, pos=p, neg=n).main()
+
+print(f'For improvements/suggestions {softinfo}')
