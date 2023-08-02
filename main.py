13be06d456f7a38d1847d1aeceffe36c8a345729
eedeidk <59429783+eedeidk@users.noreply.github.com>
2023-08-02 18:55:10 +0530

    manual score support

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tools.helpers import *
 from tools.parsers import *
 
-url = str(input("INPUT SSC CGL digialm url: "))	
+url = str(input("Paste scorecard URL: "))	
 resp = getpage(url=url)
-Parseme(resp=resp).main()
+exam=str(input('Is this CGL? (y/n)'))
+if not exam.lower() == 'y':
+    p = float(input('Positive Marks: '))
+    n = float(input('Negative Marks: '))
+    if n<0:
+        n=n*(-1)
+Parseme(resp=resp, pos=p, neg=n).main()
